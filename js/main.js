32a86336dfff8a226b8a0373a32a7c19e856c763
prositedeveloper <114259903+prositedeveloper@users.noreply.github.com>
2022-11-10 01:19:16 +0300

    kinoonline

---
+++ js/main.js
@@ -0,0 +1,429 @@
+'use strict';
+
+$(function() {
+
+	// Burger menu
+	$('#burger').on('click', function() {
+		$('#navbar').show(800);
+		$(this).hide();
+	});
+
+	$('#close').on('click', function() {
+		$('#navbar').hide(800);
+		$('#burger').show(500);
+	});
+
+	// Search
+	$('#search').on('click', function() {
+		$('#input').toggle(800);
+	});
+
+
+	// Hover link kinozal
+
+	// Row1
+	$('#item1 #square1').on('mouseover', function() {
+		$('#num1').show();
+	});
+
+	$('#item1 #square1').on('mouseout', function() {
+		$('#num1').hide();
+	});
+
+	$('#item2 #square2').on('mouseover', function() {
+		$('#num2').show();
+	});
+
+	$('#item2 #square2').on('mouseout', function() {
+		$('#num2').hide();
+	});
+
+	$('#item3 #square3').on('mouseover', function() {
+		$('#num3').show();
+	});
+
+	$('#item3 #square3').on('mouseout', function() {
+		$('#num3').hide();
+	});
+
+	$('#item4 #square4').on('mouseover', function() {
+		$('#num4').show();
+	});
+
+	$('#item4 #square4').on('mouseout', function() {
+		$('#num4').hide();
+	});
+
+	$('#item5 #square5').on('mouseover', function() {
+		$('#num5').show();
+	});
+
+	$('#item5 #square5').on('mouseout', function() {
+		$('#num5').hide();
+	});
+
+	$('#item6 #square6').on('mouseover', function() {
+		$('#num6').show();
+	});
+
+	$('#item6 #square6').on('mouseout', function() {
+		$('#num6').hide();
+	});
+
+	$('#item7 #square7').on('mouseover', function() {
+		$('#num7').show();
+	});
+
+	$('#item7 #square7').on('mouseout', function() {
+		$('#num7').hide();
+	});
+
+	$('#item8 #square8').on('mouseover', function() {
+		$('#num8').show();
+	});
+
+	$('#item8 #square8').on('mouseout', function() {
+		$('#num8').hide();
+	});
+
+	$('#item9 #square9').on('mouseover', function() {
+		$('#num9').show();
+	});
+
+	$('#item9 #square9').on('mouseout', function() {
+		$('#num9').hide();
+	});
+
+	$('#item10 #square10').on('mouseover', function() {
+		$('#num10').show();
+	});
+
+	$('#item10 #square10').on('mouseout', function() {
+		$('#num10').hide();
+	});
+
+	// Row 2
+	$('#item11 #square11').on('mouseover', function() {
+		$('#num11').show();
+	});
+
+	$('#item11 #square11').on('mouseout', function() {
+		$('#num11').hide();
+	});
+
+	$('#item12 #square12').on('mouseover', function() {
+		$('#num12').show();
+	});
+
+	$('#item12 #square12').on('mouseout', function() {
+		$('#num12').hide();
+	});
+
+	$('#item13 #square13').on('mouseover', function() {
+		$('#num13').show();
+	});
+
+	$('#item13 #square13').on('mouseout', function() {
+		$('#num13').hide();
+	});
+
+	$('#item14 #square14').on('mouseover', function() {
+		$('#num14').show();
+	});
+
+	$('#item14 #square14').on('mouseout', function() {
+		$('#num14').hide();
+	});
+
+	$('#item15 #square15').on('mouseover', function() {
+		$('#num15').show();
+	});
+
+	$('#item15 #square15').on('mouseout', function() {
+		$('#num15').hide();
+	});
+
+	$('#item16 #square16').on('mouseover', function() {
+		$('#num16').show();
+	});
+
+	$('#item16 #square16').on('mouseout', function() {
+		$('#num16').hide();
+	});
+
+	$('#item17 #square17').on('mouseover', function() {
+		$('#num17').show();
+	});
+
+	$('#item17 #square17').on('mouseout', function() {
+		$('#num17').hide();
+	});
+
+	$('#item18 #square18').on('mouseover', function() {
+		$('#num18').show();
+	});
+
+	$('#item18 #square18').on('mouseout', function() {
+		$('#num18').hide();
+	});
+
+	$('#item19 #square19').on('mouseover', function() {
+		$('#num19').show();
+	});
+
+	$('#item19 #square19').on('mouseout', function() {
+		$('#num19').hide();
+	});
+
+	$('#item20 #square20').on('mouseover', function() {
+		$('#num20').show();
+	});
+
+	$('#item20 #square20').on('mouseout', function() {
+		$('#num20').hide();
+	});
+
+	// Row 3
+	$('#item21 #square21').on('mouseover', function() {
+		$('#num21').show();
+	});
+
+	$('#item21 #square21').on('mouseout', function() {
+		$('#num21').hide();
+	});
+
+	$('#item22 #square22').on('mouseover', function() {
+		$('#num22').show();
+	});
+
+	$('#item22 #square22').on('mouseout', function() {
+		$('#num22').hide();
+	});
+
+	$('#item23 #square23').on('mouseover', function() {
+		$('#num23').show();
+	});
+
+	$('#item23 #square23').on('mouseout', function() {
+		$('#num23').hide();
+	});
+
+	$('#item24 #square24').on('mouseover', function() {
+		$('#num24').show();
+	});
+
+	$('#item24 #square24').on('mouseout', function() {
+		$('#num24').hide();
+	});
+
+	$('#item25 #square25').on('mouseover', function() {
+		$('#num25').show();
+	});
+
+	$('#item25 #square25').on('mouseout', function() {
+		$('#num25').hide();
+	});
+
+	$('#item26 #square26').on('mouseover', function() {
+		$('#num26').show();
+	});
+
+	$('#item26 #square26').on('mouseout', function() {
+		$('#num26').hide();
+	});
+
+	$('#item27 #square27').on('mouseover', function() {
+		$('#num27').show();
+	});
+
+	$('#item27 #square27').on('mouseout', function() {
+		$('#num27').hide();
+	});
+
+	$('#item28 #square28').on('mouseover', function() {
+		$('#num28').show();
+	});
+
+	$('#item28 #square28').on('mouseout', function() {
+		$('#num28').hide();
+	});
+
+	$('#item29 #square29').on('mouseover', function() {
+		$('#num29').show();
+	});
+
+	$('#item29 #square29').on('mouseout', function() {
+		$('#num29').hide();
+	});
+
+	$('#item30 #square30').on('mouseover', function() {
+		$('#num30').show();
+	});
+
+	$('#item30 #square30').on('mouseout', function() {
+		$('#num30').hide();
+	});
+
+	// Row 4
+	$('#item31 #square31').on('mouseover', function() {
+		$('#num31').show();
+	});
+
+	$('#item31 #square31').on('mouseout', function() {
+		$('#num31').hide();
+	});
+
+	$('#item32 #square32').on('mouseover', function() {
+		$('#num32').show();
+	});
+
+	$('#item32 #square32').on('mouseout', function() {
+		$('#num32').hide();
+	});
+
+	$('#item33 #square33').on('mouseover', function() {
+		$('#num33').show();
+	});
+
+	$('#item33 #square33').on('mouseout', function() {
+		$('#num33').hide();
+	});
+
+	$('#item34 #square34').on('mouseover', function() {
+		$('#num34').show();
+	});
+
+	$('#item34 #square34').on('mouseout', function() {
+		$('#num34').hide();
+	});
+
+	$('#item35 #square35').on('mouseover', function() {
+		$('#num35').show();
+	});
+
+	$('#item35 #square35').on('mouseout', function() {
+		$('#num35').hide();
+	});
+
+	$('#item36 #square36').on('mouseover', function() {
+		$('#num36').show();
+	});
+
+	$('#item36 #square36').on('mouseout', function() {
+		$('#num36').hide();
+	});
+
+	$('#item37 #square37').on('mouseover', function() {
+		$('#num37').show();
+	});
+
+	$('#item37 #square37').on('mouseout', function() {
+		$('#num37').hide();
+	});
+
+	$('#item38 #square38').on('mouseover', function() {
+		$('#num38').show();
+	});
+
+	$('#item38 #square38').on('mouseout', function() {
+		$('#num38').hide();
+	});
+
+	$('#item39 #square39').on('mouseover', function() {
+		$('#num39').show();
+	});
+
+	$('#item39 #square39').on('mouseout', function() {
+		$('#num39').hide();
+	});
+
+	$('#item40 #square40').on('mouseover', function() {
+		$('#num40').show();
+	});
+
+	$('#item40 #square40').on('mouseout', function() {
+		$('#num40').hide();
+	});
+
+	// Row 5
+	$('#item41 #square41').on('mouseover', function() {
+		$('#num41').show();
+	});
+
+	$('#item41 #square41').on('mouseout', function() {
+		$('#num41').hide();
+	});
+
+	$('#item42 #square42').on('mouseover', function() {
+		$('#num42').show();
+	});
+
+	$('#item42 #square42').on('mouseout', function() {
+		$('#num42').hide();
+	});
+
+	$('#item43 #square43').on('mouseover', function() {
+		$('#num43').show();
+	});
+
+	$('#item43 #square43').on('mouseout', function() {
+		$('#num43').hide();
+	});
+
+	$('#item44 #square44').on('mouseover', function() {
+		$('#num44').show();
+	});
+
+	$('#item44 #square44').on('mouseout', function() {
+		$('#num44').hide();
+	});
+
+	$('#item45 #square45').on('mouseover', function() {
+		$('#num45').show();
+	});
+
+	$('#item45 #square45').on('mouseout', function() {
+		$('#num45').hide();
+	});
+
+	$('#item46 #square46').on('mouseover', function() {
+		$('#num46').show();
+	});
+
+	$('#item46 #square46').on('mouseout', function() {
+		$('#num46').hide();
+	});
+
+	$('#item47 #square47').on('mouseover', function() {
+		$('#num47').show();
+	});
+
+	$('#item47 #square47').on('mouseout', function() {
+		$('#num47').hide();
+	});
+
+	$('#item48 #square48').on('mouseover', function() {
+		$('#num48').show();
+	});
+
+	$('#item48 #square48').on('mouseout', function() {
+		$('#num48').hide();
+	});
+
+	$('#item49 #square49').on('mouseover', function() {
+		$('#num49').show();
+	});
+
+	$('#item49 #square49').on('mouseout', function() {
+		$('#num49').hide();
+	});
+
+	$('#item50 #square50').on('mouseover', function() {
+		$('#num50').show();
+	});
+
+	$('#item50 #square50').on('mouseout', function() {
+		$('#num50').hide();
+	});
+
+});
